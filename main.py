68222d81563b3669ee7484bf23378cbdb9e5f897
noraniederer <ni120402@gmail.com>
2024-11-11 22:35:07 +0100

    Fixed bug for when only running 1 sims

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ new_data = False
 simN = 10
 
 # number of simulations
-sims = 6
+sims = 2
 sim_deePC = True #currently does nothing
 sim_MPC = True #currently does nothing
 
@@ -25,26 +25,25 @@ bias_factor = 1.0 # % of max bias
 m = 1    # Dimension of input (always 1)
 p = num_users   # Dimension of output
 
-# Initialize DeePC
 N = 10   # Prediction horizon
-Tini = 1   # Initial time horizon
+Tini = 1   # Initial time 
 
-#Define cost matrizes
+# Define cost matrizes
 Q = np.eye(num_steps*num_users)
 R = np.eye(num_steps)
 
 if new_data:
-    #call skript to generate data
+    # Call skript to generate data
     print(f"Starting Data generation for {num_steps} steps")
     generate_data(num_users, num_steps, sparsity_factor, bias_factor)
     print("Finished Data collection")
 else: 
     print("Skipping Data generation")
 
-# # Load the data from the .npz file
+# Load the data from the .npz file
 data = np.load('data.npz')
 
-# # Access the individual arrays by their names
+# Access the individual arrays by their names
 A = data['A']
 B = data['B']
 Lambda = data['Lambda']
@@ -132,7 +131,10 @@ fig, axes = plt.subplots(num_rows, num_columns, figsize=(4 * num_columns, 4 * nu
 # Plot for each subplot (DeepC on left side, MPC on right side)
 for i in range(sims):
     # Plot DeepC on the left column
-    ax_deepc = axes[i%3, math.floor(i/3)]  # Subplot for DeepC
+    if sims > 1:
+        ax_deepc = axes[i%3, math.floor(i/3)]  # Subplot for DeepC
+    else:
+        ax_deepc = axes[i]
     for k in range(num_users):
         handle0, = ax_deepc.plot(xevo_deepc[i, k, :], linewidth=0.5, color=[0, 0, 0])  # Plot each user's trajectory in black
     handle1, = ax_deepc.plot(np.mean(xevo_deepc[i, :, :], axis=0), linewidth=1.0, color='magenta', label='Mean Opinion')
@@ -140,11 +142,13 @@ for i in range(sims):
     ax_deepc.set_title(f"DeePC - Plot {i+1}", pad=15)
     ax_deepc.set_xlabel('Timestep')
     ax_deepc.set_ylabel('Opinion')
-    #ax_deepc.legend(loc='upper right')
     ax_deepc.grid(True)
 
     # Plot MPC on the right column
-    ax_mpc = axes[i%3, math.floor(num_columns/2) + math.floor(i/3)]  # Subplot for MPC
+    if sims > 1:
+        ax_mpc = axes[i%3, math.floor(num_columns/2) + math.floor(i/3)]  # Subplot for MPC
+    else:
+        ax_mpc = axes[sims+i]
     for k in range(num_users):
         ax_mpc.plot(xevo_mpc[i, k, :], linewidth=0.5, color=[0, 0, 0])  # Plot each user's trajectory in black
     ax_mpc.plot(np.mean(xevo_mpc[i, :, :], axis=0), linewidth=1.0, color='magenta', label='Mean Opinion')
@@ -152,13 +156,12 @@ for i in range(sims):
     ax_mpc.set_title(f"MPC - Plot {i+1}", pad=15)
     ax_mpc.set_xlabel('Timestep')
     ax_mpc.set_ylabel('Opinion')
-    #ax_mpc.legend(loc='upper right')
     ax_mpc.grid(True)
 
-
+# Add legend and title
 fig.legend([handle0, handle1, handle2], ["State i's opinion", "Mean opinion", "Proposed Input by Algo"], loc='upper center', ncol=3, fontsize=12, bbox_to_anchor=(0.5, 0.95), frameon=False)
 fig.suptitle(f"Comparison of DeePC and MPC Opinion Trajectories (num_steps={num_steps}, Tini={Tini})", fontsize=16, fontweight='bold')
 
 # Adjust the layout and spacing between subplots
-plt.tight_layout(pad=sims/1.5)  # Add more padding between subplots
+plt.tight_layout(pad= 2 + sims/2)  # Add more padding between subplots
 plt.show()
